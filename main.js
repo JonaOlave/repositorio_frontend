cd071e7f214dedc857f95b3ac9bdbf31ea71a17d
JonaOlave <jonathan.olave@outlook.com>
2021-05-14 05:29:22 -0400

    Agregando archivos para iniciarlar repositorio

---
+++ main.js
@@ -0,0 +1,7 @@
+// Expresiones Arismeticas
+
+let suma = 5 + 4; // Suma
+let resta = 5 - 4; // Resta
+let multiplicacion = 5 * 4 // multiplicacion
+let division = 5 / 4 // division
+document.write(suma)
